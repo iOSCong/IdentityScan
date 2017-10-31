ac3ce3faa54c87ff8370ece03d95a6ea1b5f4918
MCEJ <mcej@MCEJdeMacBook-Pro.local>
2017-10-31 14:03:02 +0800

    Initial Commit

---
+++ IdentityScan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IdentityScan
+//
+//  Created by MCEJ on 2017/10/31.
+//  Copyright © 2017年 MCEJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
